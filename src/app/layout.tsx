28d91c49f626defb0ab6fe6acaf51b29d8e50d2b
Jonathan Miller <jon@jmill.dev>
2025-05-17 12:50:16 -0400

    sphere looks normal again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,14 +15,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <div id="app">
-          <header className="overflow-hidden whitespace-nowrap overflow-ellipsis bg-gray-800 text-white p-4">
+        <div
+          id="app"
+          className="grid h-screen grid-rows-[80px_1fr_80px] gap-2 p-2"
+        >
+          <header className="overflow-hidden whitespace-nowrap overflow-ellipsis bg-gray-800 text-white p-4 flex justify-between items-center border border-gray-700">
             <div className="logo">JMILL</div>
             <div className="status">SYSTEM STATUS: NORMAL</div>
             <div className="time">T-MINUS 24:15:37</div>
           </header>
-          <main className="flex-grow">{children}</main>
-          <footer className="bg-gray-800 text-white p-4">
+          <main className="flex-grow relative border border-gray-700 bg-opacity-70 bg-gray-900">
+            {children}
+          </main>
+          <footer className="bg-gray-800 text-white p-4 border border-gray-700">
             <div className="command-line">
               <span className="command-prompt">CMD&gt;</span>
               <input
