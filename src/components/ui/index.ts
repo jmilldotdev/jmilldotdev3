1f21867cbd7f07cdea0d7127488442a8deb594f1
Jonathan Miller <jon@jmill.dev>
2025-05-17 18:17:24 -0400

    fix build

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,4 +6,3 @@ export { default as HexagonGrid } from "./HexagonGrid";
 export { default as DataGrid } from "./DataGrid";
 export { default as DataSection } from "./DataSection";
 export { default as Terminal } from "./Terminal";
-export { default as CommandLine } from "./CommandLine";
